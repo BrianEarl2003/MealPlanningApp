ab90c7ddc15658b7840009e88b2d76bc80dc989f
lucas <mor18008@byui.edu>
2021-05-31 17:11:26 -0600

    Trying out MongoDB

--- displayRecipes.js
+++ displayRecipes.js
@@ -1,17 +1,40 @@
-fetch('https://v2.jokeapi.dev/joke/Any')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    appendData(data);
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+const {MongoClient} = require('mongodb');
 
-  function appendData(data) {
-    var mainContainer = document.getElementById("myData");
-      var div = document.createElement("div");
-      div.innerHTML = 'Setup: ' + data.setup + ' Delivery: ' + data.delivery;
-      mainContainer.appendChild(div);
+async function main() {
+	const uri = "mongodb+srv://user:user123@cluster0.4bkrd.mongodb.net/recipeList?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  try {
+    await client.connect();
+
+    await listRecipes(client);
+  } catch (e) {
+    console.error(e);
   }
+
+  finally {
+  await client.close();
+  }
+
+
+}
+
+async function listRecipes(client) {
+  const cursor = client.db("recipes").collection("recipeList").find();
+
+  const results = await cursor.toArray();
+
+  if (results.length > 0) {
+    results.forEach((result, i) => {
+      console.log();
+      console.log(`${i+1}. name: ${result.recipeName}`);
+      console.log(`ingredients: ${result.ingredients}`);
+      console.log(`prepMethod: ${result.prepMethod}`);
+    });
+  } else {
+    console.log("Fatal Failure. No recipes found!!!");
+  }
+
+}
+
+main().catch(console.error);
